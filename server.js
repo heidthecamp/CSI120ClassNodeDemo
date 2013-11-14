5f11f5879b3a375f26364d78621f43f5e9835e54
OpenShift guest <heidthecamp@gmail.com>
2013-11-14 18:41:26 -0500

    h

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.json(vaca);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var vaca = {
+    "name" : "Tim Heidcamp",
+    "vacation" : {
+        "places" : ["45.64, -73.63","40.760, -73.973","32.750, -117.07"]
+    }
+}
